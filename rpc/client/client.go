08de8e71d1203cb9e6e2518cc69536adaaf3c7b5
Octavian Mocanu <octavian.mocanu@letgo.com>
2018-04-17 16:46:09 +0200

    client cleaned having now only rpc_client

--- rpc/client/client.go
+++ rpc/client/client.go
@@ -10,41 +10,33 @@ import (
 )
 
 type Bloomfilter struct {
-	client []*rpc.Client
+	client *rpc.Client
 }
 
-func New(address ...string) (*Bloomfilter, error) {
-	clients := make([]*rpc.Client, len(address))
-	for i, addr := range address {
-		client, err := rpc.Dial("tcp", addr)
-		if err != nil {
-			return nil, err
-		}
-		clients[i] = client
+func New(address string) (*Bloomfilter, error) {
+	client, err := rpc.Dial("tcp", address)
+	if err != nil {
+		return nil, err
 	}
-	return &Bloomfilter{clients}, nil
+	return &Bloomfilter{client}, nil
 }
 
 func (b *Bloomfilter) Add(elem []byte) {
-	for _, c := range b.client {
-		var addOutput rpc_bf.AddOutput
-		if err := c.Call("BloomfilterRPC.Add", rpc_bf.AddInput{[][]byte{elem}}, &addOutput); err != nil {
-			fmt.Println("error on adding remote bloomfilter:", err.Error())
-		}
+	var addOutput rpc_bf.AddOutput
+	if err := b.client.Call("BloomfilterRPC.Add", rpc_bf.AddInput{[][]byte{elem}}, &addOutput); err != nil {
+		fmt.Println("error on adding bloomfilter:", err.Error())
 	}
 }
 
 func (b *Bloomfilter) Check(elem []byte) bool {
-	for _, c := range b.client {
-		var checkOutput rpc_bf.CheckOutput
-		if err := c.Call("BloomfilterRPC.Check", rpc_bf.CheckInput{[][]byte{elem}}, &checkOutput); err != nil {
-			fmt.Println("error on check remote bloomfilter:", err.Error())
+	var checkOutput rpc_bf.CheckOutput
+	if err := b.client.Call("BloomfilterRPC.Check", rpc_bf.CheckInput{[][]byte{elem}}, &checkOutput); err != nil {
+		fmt.Println("error on check bloomfilter:", err.Error())
+		return false
+	}
+	for _, v := range checkOutput.Checks {
+		if !v {
 			return false
-		}
-		for _, v := range checkOutput.Checks {
-			if !v {
-				return false
-			}
 		}
 	}
 	return true
@@ -55,20 +47,15 @@ func (b *Bloomfilter) Union(that interface{}) (float64, error) {
 	if !ok {
 		return -1.0, errors.New("invalide argument to Union, expected rotate.Bloomfilter")
 	}
-	var capacity float64
-	for _, c := range b.client {
-		var unionOutput rpc_bf.UnionOutput
-		if err := c.Call("BloomfilterRPC.Union", rpc_bf.UnionInput{v}, &unionOutput); err != nil {
-			fmt.Println("error on union remote bloomfilter:", err.Error())
-			return -1.0, err
-		}
-		capacity += unionOutput.Capacity
+	var unionOutput rpc_bf.UnionOutput
+	if err := b.client.Call("BloomfilterRPC.Union", rpc_bf.UnionInput{v}, &unionOutput); err != nil {
+		fmt.Println("error on union bloomfilter:", err.Error())
+		return -1.0, err
 	}
-	return capacity / float64(len(b.client)), nil
+
+	return unionOutput.Capacity, nil
 }
 
 func (b *Bloomfilter) Close() {
-	for _, c := range b.client {
-		c.Close()
-	}
+	b.client.Close()
 }
